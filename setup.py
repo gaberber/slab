69093579859cb82cd02d77bd490f96d8c493c2dc
Gaberber <cnhajzwgz@gmail.com>
2025-08-22 17:14:10 -0700

    make it 'pip install -e' -able

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 setup.py - a module to allow package installation
 """
 
-from distutils.core import setup
+
+from setuptools import setup, find_packages
 
 NAME = "slab"
 VERSION = "0.1"
@@ -14,10 +15,14 @@ DESCRIPTION = "This package is used for Schuster Lab experiments"
 AUTHOR = "David Schuster"
 AUTHOR_EMAIL = "david.schuster@uchicago.edu"
 
-setup(author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      description=DESCRIPTION,
-      install_requires=DEPENDENCIES,
-      name=NAME,
-      version=VERSION,
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    install_requires=DEPENDENCIES,
+    packages=find_packages(include=["*"], exclude=["__pycache__", "*.pyc"]),
+    include_package_data=True,
+    zip_safe=False,
 )
